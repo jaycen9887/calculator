883a44ceeba3e41d81eca903fd3ce822ffcf593f
Jaycen <jaycen9887@gmail.com>
2017-06-10 11:55:00 -0400

    initial add of files

---
+++ script.js
@@ -0,0 +1,14 @@
+var numId;
+var num;
+
+//panel body on click
+
+$("#panel-body").on("click", function(){
+    
+    numId = ($(this).attr("id"));
+    num = parseInt(numId.text());
+    
+    //if element.text === num 
+        // store in num array
+    
+});
